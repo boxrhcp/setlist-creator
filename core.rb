9a46ba80c2293b86d8b9baeb3cfdb5b160d50cf2
Fayán Leonardo Pardo Ladino <fayanpardo@gmail.com>
2017-02-18 18:44:26 +0100

    connection setlist api

---
+++ core.rb
@@ -0,0 +1,31 @@
+load 'setlistfm.rb'
+require 'net/https'
+require 'uri'
+#...
+
+
+#read a resource from a REST url
+def main
+	puts 'try it'
+	apiKey = 'fe0a144d-090a-475e-904d-02b50cbd75be'
+	url = URI.parse("https://api.setlist.fm/rest/0.1/artist/65f4f0c5-ef9e-490c-aee3-909e7ae6b2ab.json?key="+apiKey)
+
+	request = Net::HTTP::Get.new(url.request_uri)
+
+	http = Net::HTTP.new(url.host, url.port)
+	http.use_ssl = true if url.scheme == 'https'
+	#set up additional http stuff...
+	res = http.start do |ht|
+	  ht.request(request)
+	end
+
+	#result = Setlist.from_json(JSON.parse(res.body)
+	result = JSON.parse(res.body)
+
+	puts result
+
+end
+
+main
+
+#handle the result as needed...
